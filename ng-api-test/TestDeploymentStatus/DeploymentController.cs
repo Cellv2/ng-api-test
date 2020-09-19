be9e0b6e21873acceecd66177e54673fc2af044b
Cellv2 <22135118+Cellv2@users.noreply.github.com>
2020-09-19 20:41:22 +0100

    Make the returned data update each time

--- ng-api-test/TestDeploymentStatus/DeploymentController.cs
+++ ng-api-test/TestDeploymentStatus/DeploymentController.cs
@@ -11,21 +11,37 @@ namespace ng_api_test.Controllers
     [Route("[controller]")]
     public class DeploymentController : ControllerBase
     {
-        private Deployment deployment = new Deployment
+
+        private DeploymentOperationStatus GetRandomStatus()
         {
-            Name = "Test Deployment",
-            CurrentOperationStatus = DeploymentOperationStatus.Complete,
-            CurrentOperationCompletionPercentage = 0,
-        };
+            var random = new Random().Next(4);
+            switch (random)
+            {
+                case 0:
+                    return DeploymentOperationStatus.Complete;
+
+                case 1:
+                    return DeploymentOperationStatus.Enqueued;
+
+                case 2:
+                    return DeploymentOperationStatus.InProgress;
+                
+                case 3:
+                    return DeploymentOperationStatus.Failed;
+
+                default:
+                    return DeploymentOperationStatus.Complete;
+            }
+        }
 
         [HttpGet]
         public IEnumerable<Deployment> Get()
         {
             return Enumerable.Range(1, 3).Select(index => new Deployment
             {
-                Name = $"Deployment {index}",
-                CurrentOperationCompletionPercentage = 50,
-                CurrentOperationStatus = DeploymentOperationStatus.Complete
+                Name = $"Deployment {index} - {Guid.NewGuid()}",
+                CurrentOperationCompletionPercentage = Convert.ToByte(new Random().Next(100)),
+                CurrentOperationStatus = GetRandomStatus()
             }).ToArray();
         }
     }
